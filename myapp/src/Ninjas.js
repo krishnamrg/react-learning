b96fe1d2611f055113d88fe540230a9cb107760a
krishnamg <krishnamrajgoud@gmail.com>
2019-11-26 15:32:48 +0530

    changed Ninjas component to use filter and destructuring and inlined all template inside main div

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,18 +3,35 @@ import React from "react";
 //this has been converted to functional component(UI component) from class component.
 
 //const Ninjas = ({ninjas}) => { THIS LINE WILL ALSO WORK, ITS USING DESTRUCTURING.
-const Ninjas = props => {
-  const { ninjas } = props;
-  const ninjaList = ninjas.map(ninja => {
-    return (
-      <div className="ninja" key={ninja.id}>
-        <div>Name: {ninja.name}</div>
-        <div>Belt: {ninja.belt}</div>
-        <div>Age: {ninja.age}</div>
-      </div>
-    );
-  });
-  return <div className="ninja-list">{ninjaList}</div>;
+// const Ninjas = props => {
+//   const { ninjas } = props;
+//   const ninjaList = ninjas.map(ninja => {
+//     return (
+//       <div className="ninja" key={ninja.id}>
+//         <div>Name: {ninja.name}</div>
+//         <div>Belt: {ninja.belt}</div>
+//         <div>Age: {ninja.age}</div>
+//       </div>
+//     );
+//   });
+//   return <div className="ninja-list">{ninjaList}</div>;
+
+//Changed from above T-15 to this T-16
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div className="ninja-list"> {
+      ninjas.map(ninja => {
+        return (
+          <div className="ninja" key={ninja.id}>
+            <div>Name: {ninja.name}</div>
+            <div>Belt: {ninja.belt}</div>
+            <div>Age: {ninja.age}</div>
+          </div>
+        );
+      })
+    }
+    </div>
+  );
 };
 
 export default Ninjas;
