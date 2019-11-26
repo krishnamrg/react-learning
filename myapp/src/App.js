330ad0290897670ef267f7cb5fce547314f93a58
krishnamg <krishnamrajgoud@gmail.com>
2019-11-26 16:49:26 +0530

    added AddNinja Component form and added it to parent component

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,21 +1,26 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Ninjas from "./Ninjas";
+import AddNinja from "./AddNinja";
 
 // this is a class component and it maintains state.
 class App extends Component {
-
   state = {
-    ninjas : [
-      {name:'Ryu', age:30, belt:'Black', id:1},
-      {name:'Yoshi', age:32, belt:'Red', id:3},
-      {name:'Crystal', age:33, belt:'Yellow', id:2}
+    ninjas: [
+      { name: "Ryu", age: 30, belt: "Black", id: 1 },
+      { name: "Yoshi", age: 32, belt: "Red", id: 3 },
+      { name: "Crystal", age: 33, belt: "Yellow", id: 2 }
     ]
-  }
+  };
 
   render() {
     return (
       <div className="App">
-        <Ninjas ninjas={this.state.ninjas}> here are the Ninjas</Ninjas>
+        <div>
+          <Ninjas ninjas={this.state.ninjas}> here are the Ninjas</Ninjas>
+        </div>
+        <div>
+          <AddNinja></AddNinja>
+        </div>
       </div>
     );
   }
